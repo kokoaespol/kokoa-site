a332ed79d7b19c739d7f794b0cfafe255b809391
Alexander Goussas <goussasalexander@gmail.com>
2024-10-28 19:03:39 -0500

    add gokul soumya as speaker in cli week

--- src/lib/components/Speaker.svelte
+++ src/lib/components/Speaker.svelte
@@ -10,18 +10,19 @@
 	const { name, image_url, date, time, title, topic }: Props = $props();
 </script>
 
-<div class="my-4 flex w-4/5 gap-6 rounded-2xl bg-neutral-800 px-6 py-4">
-	<img src={image_url} alt={name} class="w-[100px] rounded-full" />
-	<div class="flex flex-col gap-2">
-		<div class="mb-2 flex flex-col gap-1">
-			<span>{name}</span>
-			<small class="text-lime-600">{title}</small>
+<div class="my-4 flex w-fit flex-col items-center gap-6 rounded-2xl bg-neutral-800 px-6 py-4">
+	<img src={image_url} alt={name} class="w-[150px] self-center rounded-full" />
+	<div class="flex flex-col gap-6 px-6 py-4">
+		<div class="h-100 flex max-w-[250px] items-center text-center">
+			<span class="text-xl">{topic}</span>
 		</div>
-		<span>📅 {date}</span>
-		<span>🕙 {time}</span>
-	</div>
-	<div class="h-100 w-[1px] bg-lime-600"></div>
-	<div class="h-100 flex items-center text-center">
-		<span class="text-xl">{topic}</span>
+		<div class="flex flex-col gap-2">
+			<div class="mb-2 flex flex-col gap-1 text-center">
+				<span>{name}</span>
+				<small class="text-lime-600">{title}</small>
+			</div>
+			<span>📅 {date}</span>
+			<span>🕙 {time}</span>
+		</div>
 	</div>
 </div>
