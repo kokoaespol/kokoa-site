7ca5d09a42a39654319764f237ee40b3eb4050f4
Dayanara Bone <dayanara.bone@uees.edu.ec>
2025-05-05 01:11:32 -0500

    refactor: changes in events structure (#98)
    * refactor: changes in events structure
    
    * fix: add relevant information about events
    
    * chore: fix lint error
    
    * chore: fix linting errors
    
    * chore: fix linting errors
    
    * refactor: fix function names and sort events using toSorted
    
    ---------
    
    Co-authored-by: Noelia Alexandra Pasaca Paladines <pasacanoelia@gmail.com>

---
+++ src/routes/(heading)/events/[id=number]/+page.ts
@@ -0,0 +1,18 @@
+import type { PageLoad } from "./$types";
+import events from "$lib/data/events.json";
+import { error } from "@sveltejs/kit";
+
+export const load = (async ({ params }) => {
+	const id = parseInt(params.id, 10);
+	const event = events.find((event) => event.id === id);
+
+	if (!event) {
+		throw error(404, `Evento no encontrado`);
+	}
+
+	return {
+		title: event.name,
+		description: event.description,
+		event,
+	};
+}) satisfies PageLoad;
