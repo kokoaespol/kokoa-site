16ec810ff492915c5a4f7c06dd72d83723b58a45
Adrian Delgado <git@adrianjdelgado.com>
2025-05-10 18:01:57 -0500

    chore: apply `pnpx @tailwindcss/upgrade@latest`

--- src/routes/(heading)/+layout.svelte
+++ src/routes/(heading)/+layout.svelte
@@ -12,11 +12,11 @@
 			<span class="ml-auto w-24 sm:w-56"></span>
 		</h1>
 		<div class="absolute inset-y-0 right-10 w-5 bg-gray-950 sm:w-7"></div>
-		<div class="absolute inset-y-0 right-16 w-5 bg-gray-950 sm:right-[4.75rem] sm:w-7"></div>
-		<div class="absolute inset-y-0 right-[5.5rem] w-5 bg-gray-950 sm:right-28 sm:w-7"></div>
-		<div class="absolute inset-y-0 right-[9.25rem] hidden w-7 bg-gray-950 sm:block"></div>
-		<div class="absolute inset-y-0 right-[11.5rem] hidden w-7 bg-gray-950 sm:block"></div>
-		<div class="absolute inset-y-0 right-[13.75rem] hidden w-7 bg-gray-950 sm:block"></div>
+		<div class="absolute inset-y-0 right-16 w-5 bg-gray-950 sm:right-19 sm:w-7"></div>
+		<div class="absolute inset-y-0 right-22 w-5 bg-gray-950 sm:right-28 sm:w-7"></div>
+		<div class="absolute inset-y-0 right-37 hidden w-7 bg-gray-950 sm:block"></div>
+		<div class="absolute inset-y-0 right-46 hidden w-7 bg-gray-950 sm:block"></div>
+		<div class="absolute inset-y-0 right-55 hidden w-7 bg-gray-950 sm:block"></div>
 		<div class="absolute inset-y-0 right-64 hidden w-7 bg-gray-950 sm:block"></div>
 	</CenterContainer>
 </div>
