6416675f1d8a5f10e651b7864cdedbef0f945f82
Adrian Delgado <git@adrianjdelgado.com>
2025-05-10 19:11:44 -0500

    fix: avoid collisions with tailwindcss variables

--- src/routes/(heading)/events/[id=number]/+page.svelte
+++ src/routes/(heading)/events/[id=number]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import CenterContainer from "$lib/components/center-container.svelte";
 	import Tag from "$lib/components/tag.svelte";
-	import { Dot, Globe, Instagram } from "lucide-svelte";
+	import instagram from "$lib/assets/icons/instagram-green.svg";
+	import { Dot, Globe } from "lucide-svelte";
 
 	let { data } = $props();
 
@@ -25,21 +26,21 @@
 
 {#snippet subtitle(text: string)}
 	<h2 class="mt-4 flex items-center gap-1 text-3xl font-bold">
-		<span aria-hidden="true" class="text-green italic">#</span>
+		<span aria-hidden="true" class="text-kokoa-lime1 italic">#</span>
 		{text}
 	</h2>
 {/snippet}
 
 <CenterContainer class="py-12">
 	<section>
-		<h2 class="font-fira text-xl text-green">Evento</h2>
+		<h2 class="font-fira text-xl text-kokoa-lime1">Evento</h2>
 		<div class="mt-2 flex flex-wrap gap-8">
 			<span class="font-fira">{data.event.name}</span>
 		</div>
 	</section>
 	<section class="mt-8 rounded-2xl bg-neutral-800 p-8">
 		<div class="flex flex-wrap items-center gap-1 font-fira">
-			<span class="text-green">{data.event.date}</span>
+			<span class="text-kokoa-lime1">{data.event.date}</span>
 			<Dot />
 			{#each data.event.categories as category (category)}
 				<Tag {category} />
@@ -71,7 +72,7 @@
 		<div class="mt-6 flex flex-col gap-4">
 			{#if data.event.web_url}
 				<a class="flex gap-1 font-fira hover:underline" rel="external" href={data.event.web_url}>
-					<Globe class="size-6 text-green" />
+					<Globe class="size-6 text-kokoa-lime1" />
 					{data.event.web_page}
 				</a>
 			{/if}
@@ -82,7 +83,7 @@
 					rel="external"
 					href={data.event.instagram_url}
 				>
-					<Instagram class="size-6 text-green" />
+					<img src={instagram} alt="Instagram" class="size-6" width="24" height="24" />
 					{data.event.instagram_username}
 				</a>
 			{/if}
